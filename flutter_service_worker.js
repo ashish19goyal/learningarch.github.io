d0b0e9c498c5bc5a7cae068299b4ae0d4c049b58
Ashish Goyal <ashish.19goyal@gmail.com>
2020-07-19 01:29:00 +0530

    infinite scroll

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "91f50bc1c13392e128a50c28ca7d3530",
-"main.dart.js": "44ec95660ce4a096d7e2bf18cd2bf332",
+"main.dart.js": "3e4be3a296337b79588c55b6f97397ed",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/AssetManifest.json": "e3ee6ff05c809de60c912c5091cd0710",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
@@ -15,7 +15,6 @@ const RESOURCES = {
 "assets/assets/images/logo.jpg": "3042314cbbd80e61ec3dbd7d4b569d83",
 "assets/assets/images/1.0x/logo.jpg": "221597a300b57213a3ff3d4c6ba32274",
 "assets/assets/images/3.0x/logo.jpg": "bb3eb182b6fac6fab8f11757d0da5330",
-"assets/LICENSE": "da693755d0b91d2f1168bf441c806a50",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "icons/Icon-300.png": "077107d4288b0d2fd6e8189ac99ea058",
 "index.html": "94028219e6fb121d74e68ed6e94b5888",
