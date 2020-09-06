8c3dcac0c63416b12d6012451e83ff1b53ba745b
Ashish Goyal <ashish.19goyal@gmail.com>
2020-09-07 00:22:17 +0530

    auto-commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "475330f5670191542c63ec7529ad0e3d",
-"main.dart.js": "ae1835236b7a17258e7e1f0527831858",
+"main.dart.js": "cef7e49db371f698f645a14de94b4fc3",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.json": "e3ee6ff05c809de60c912c5091cd0710",
 "assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
